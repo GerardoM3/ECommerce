152667fe46d0b8a60ce2b69fe4174c5f513a40e4
Carlos Armando <80137888+carlos-arman@users.noreply.github.com>
2022-11-10 21:18:22 -0800

    Producto (Vista Cliente)

---
+++ app/src/main/java/com/sis21a/e_commerce/vista_productos.java
@@ -0,0 +1,14 @@
+package com.sis21a.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vista_productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_productos);
+    }
+}
