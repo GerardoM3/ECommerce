38e45529b38055f247da646e4a1fc2bf28b1eeee
Carlos Armando <80137888+carlos-arman@users.noreply.github.com>
2022-11-10 17:40:23 -0800

    Clase y vistas --Ver tiendas--

---
+++ app/src/main/java/com/sis21a/e_commerce/Vistas_tienda.java
@@ -0,0 +1,14 @@
+package com.sis21a.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Vistas_tienda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vistas_tienda);
+    }
+}
