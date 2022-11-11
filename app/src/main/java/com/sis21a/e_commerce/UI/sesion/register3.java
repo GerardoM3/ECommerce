bf3d94239015bf34aabe9670e6b81af7e9063eb1
GerardoM3 <gerardomonroy13@gmail.com>
2022-11-10 21:07:06 -0600

    Registro 3 agregado (Diseño XML y clase Java)

---
+++ app/src/main/java/com/sis21a/e_commerce/UI/sesion/register3.java
@@ -0,0 +1,15 @@
+package com.sis21a.e_commerce.UI.sesion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.sis21a.e_commerce.R;
+
+import android.os.Bundle;
+
+public class register3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register3);
+    }
+}
