ee736d7a161833c913db385090c15199455d0b95
yuleydidelgado <yuleydiestefani@gmail.com>
2022-11-15 14:02:18 -0600

    btn_cat-prod

--- app/src/main/java/com/example/ecomercecopy/MainActivity.java
+++ app/src/main/java/com/example/ecomercecopy/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.ecomercecopy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.sis21a.e_commerce.R;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnMainCategoria, btnMainProducto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vistascat);
+
+        btnMainCategoria=findViewById(R.id.btnMainCategoria);
+        btnMainProducto=findViewById(R.id.btnMainProducto);
+
     }
 }
