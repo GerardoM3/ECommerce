deb3ab7bcdf827217170adb43db578b024351e49
Carlos Armando <80137888+carlos-arman@users.noreply.github.com>
2022-11-15 14:04:16 -0800

    Modificar Producto

--- app/src/main/java/com/sis21a/e_commerce/Producto/modificar_producto.java
+++ app/src/main/java/com/sis21a/e_commerce/Producto/modificar_producto.java
@@ -1,15 +1,32 @@
 package com.sis21a.e_commerce.Producto;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sis21a.e_commerce.R;
 
 public class modificar_producto extends AppCompatActivity {
+    EditText etNombreProducto,etDescripcionProducto,etStockProducto,etPrecioProducto;
+    Spinner spCategoriaProducto;
+    CheckBox cbEstadoProducto;
+    Button btnCancelarP,btnActualizarPro;
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar);
+
+        etNombreProducto = findViewById(R.id.etNombreProducto);
+        etDescripcionProducto = findViewById(R.id.etDescripcionProducto);
+        etStockProducto = findViewById(R.id.etStockProducto);
+        etPrecioProducto = findViewById(R.id.etPrecioProducto);
+        spCategoriaProducto = findViewById(R.id.spCategoriaProducto);
+        cbEstadoProducto = findViewById(R.id.cbEstadoProducto);
+        btnCancelarP = findViewById(R.id.btnCancelarP);
+        btnActualizarPro = findViewById(R.id.btnActualizarPro);
     }
 }
