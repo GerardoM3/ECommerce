007f6553ca5fdadfac2dbfe0a7fadbe735b400ab
Carlos Armando <80137888+carlos-arman@users.noreply.github.com>
2022-11-16 12:42:46 -0800

    Dto negocio

--- app/src/main/java/com/sis21a/e_commerce/Dto/dto_negocio.java
+++ app/src/main/java/com/sis21a/e_commerce/Dto/dto_negocio.java
@@ -1,5 +1,51 @@
 package com.sis21a.e_commerce.Dto;
 
 public class dto_negocio {
+    public int getId_negocio() {
+        return id_negocio;
+    }
+
+    public void setId_negocio(int id_negocio) {
+        this.id_negocio = id_negocio;
+    }
+
+    public String getNombre_negocio() {
+        return nombre_negocio;
+    }
+
+    public void setNombre_negocio(String nombre_negocio) {
+        this.nombre_negocio = nombre_negocio;
+    }
+
+    public int getTipo_negocio() {
+        return tipo_negocio;
+    }
+
+    public void setTipo_negocio(int tipo_negocio) {
+        this.tipo_negocio = tipo_negocio;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public dto_negocio() {
+    }
+
+    public dto_negocio(int id_negocio, String nombre_negocio, int tipo_negocio, int id_usuario) {
+        this.id_negocio = id_negocio;
+        this.nombre_negocio = nombre_negocio;
+        this.tipo_negocio = tipo_negocio;
+        this.id_usuario = id_usuario;
+    }
+
+    int id_negocio;
+    String nombre_negocio;
+    int tipo_negocio;
+    int id_usuario;
 
 }
